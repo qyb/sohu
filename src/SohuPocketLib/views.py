8470ca71191007f51416c0ceb858105010c57370
bonbondirac <diracfangustc@gmail.com>
2011-12-31 13:50:07 +0800

    first commmit
    project sohupocketlib
    
    Signed-off-by: bonbondirac <diracfangustc@gmail.com>

---
+++ src/SohuPocketLib/views.py
@@ -0,0 +1,16 @@
+'''
+Created on 2011-12-29
+
+@author: diracfang
+'''
+from django.http import HttpResponse
+
+def passport(request):
+    info = ['HTTP_X_SOHUPASSPORT_UID',
+            'HTTP_X_SOHUPASSPORT_USERID',
+            'HTTP_X_SOHUPASSPORT_UUID']
+    page = '<br />'.join([i + ' : ' + str(request.META[i]) for i in info])
+    return HttpResponse(page)
+
+def home(request):
+    return HttpResponse('welcome home')
