46564153972a89244ea265c35dc2312d1724980b
riven <riven@rivenmatoMacBook-Pro.local>
2012-01-08 17:13:51 +0800

    add cache and session backend

--- src/SohuPocketLib/settings.py
+++ src/SohuPocketLib/settings.py
@@ -131,6 +131,8 @@ INSTALLED_APPS = (
     'SohuPocketLib.storage',
 )
 
+CACHE_BACKEND = 'locmem://'
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
