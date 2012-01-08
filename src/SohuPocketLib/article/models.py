97227aa238f38a61bec6d50123477c558067c053
riven <riven@rivenmatoMacBook-Pro.local>
2012-01-08 15:45:16 +0800

    add model for user article and add function for s3

--- src/SohuPocketLib/article/models.py
+++ src/SohuPocketLib/article/models.py
@@ -1,4 +1,58 @@
 # -*- coding: utf-8 -*-
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.core.cache import cache
+
+class MultiDB(models.Model):
+    user_id = models.IntegerField()
+
+    def save(self):
+        if self.user_id <= LIMIT_USERS_ONE_DB:
+            super(MultiDB,self).save(using='default')
+        elif self.user_id <= 2*LIMIT_USERS_ONE_DB:
+            super(MultiDB,self).save(using='second')
+        elif self.user_id <= 3*LIMIT_USERS_ONE_DB:
+            super(MultiDB,self).save(using='third')
+
+    def delete(self):
+        if self.user_id <= LIMIT_USERS_ONE_DB:
+            super(MultiDB,self).delete(using='default')
+        elif self.user_id <= 2*LIMIT_USERS_ONE_DB:
+            super(MultiDB,self).delete(using='second')
+        elif self.user_id <= 3*LIMIT_USERS_ONE_DB:
+            super(MultiDB,self).delete(using='third')
+
+    class Meta:
+        abstract = True
+
+class MyArticleInstance(MultiDB):
+    #user_id = models.IntegerField()
+    key = models.CharField(max_length=32)        #从S3获取静态文件
+    title = models.CharField(max_length=512)     #文章的标题
+    url = models.CharField(max_length=512)       #文章原始URL
+    is_read = models.BooleanField(default=False) #是否已读   
+    cover = models.CharField(max_length=128)     #网页HEADER图片
+    is_star = models.BooleanField(default=False) #是否标记星标
+
+    def __unicode__(self):
+        return u'%s,%s' % (self.user_id, self.title)
+    
+    def update_cache(self):
+        key = CACHE_KEY_USER_ARTICLE_INSTANCE % (self.user_id, self.key)
+        cache.set(key, self)
+
+    def delete_cache(self):
+        key = CACHE_KEY_USER_ARTICLE_INSTANCE % (self.user_id, self.key)
+        cache.delete(key)
+
+    def save(self):
+        super(MyArticleInstance, self).save()
+        self.update_cache()
+
+    def delete(self):
+        self.delete_cache()
+        super(MyArticleInstance, self).delete()
+    
+    class Meta:
+        unique_together = (('user_id', 'key'),)
+        db_table = 'user_article_instance'
--- src/SohuPocketLib/storage/helper.py
+++ src/SohuPocketLib/storage/helper.py
@@ -8,78 +8,103 @@ def bulid_connect_s3():
     s3 = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
     return s3
 
-def get_or_create_bucket(bucket_name, location=Location.DEFAULT):
+def get_or_create_bucket(bucket_name, policy='public-read', location=Location.DEFAULT):
     s3 = bulid_connect_s3()
     bucket = s3.lookup(bucket_name)
     if not bucket:
         try:
-            bucket = s3.create_bucket(bucket_name, location=location)
+            bucket = s3.create_bucket(bucket_name, policy=policy, location=location)
+            bucket.set_canned_acl('public-read')
         except s3.provider.storage_create_error, e:
             print 'Bucket (%s) is owned by another user' % bucket_name
     return bucket
 
-def get_private_data_to_string(bucket_name, key_name):
+def get_data_to_string(bucket_name, key_name):
     #get_contents_to_filename,get_file
-    bucket = get_or_create_bucket(bucket_name, location=Location.DEFAULT)
+    bucket = get_or_create_bucket(bucket_name)
     key = bucket.lookup(key_name)
     if not key:
         return None
     return key.get_contents_as_string()
 
-def get_private_data_to_filename(bucket_name, key_name, filename):
+def get_data_to_filename(bucket_name, key_name, filename):
     #get_contents_to_filename,get_file
-    bucket = get_or_create_bucket(bucket_name, location=Location.DEFAULT)
+    bucket = get_or_create_bucket(bucket_name)
     key = bucket.lookup(key_name)
     if not key:
         return None
     return key.get_contents_to_filename(filename)
 
-def store_private_data_from_filename(bucket_name, key_name, path_source_file, metadata=None):
-    bucket = get_or_create_bucket(bucket_name, location=Location.DEFAULT)
+def store_data_from_filename(bucket_name, key_name, path_source_file, metadata=None, policy='public-read'):
+    bucket = get_or_create_bucket(bucket_name, policy)
     key = bucket.new_key(key_name)
-    key.set_contents_from_filename(path_source_file)
+    key.set_contents_from_filename(path_source_file, policy=policy)
     if metadata:
         key.metadata.update(metadata)
     return key
 
-def store_private_data_from_stream(bucket_name, key_name, stream, metadata=None):
-    bucket = get_or_create_bucket(bucket_name, location=Location.DEFAULT)
+def store_data_from_stream(bucket_name, key_name, stream, metadata=None, policy='public-read'):
+    bucket = get_or_create_bucket(bucket_name, policy)
     key = bucket.new_key(key_name)
-    key.set_contents_from_stream(stream)
+    key.set_contents_from_stream(stream, policy=policy)
     if metadata:
         key.metadata.update(metadata)
     return key
 
-def store_private_data_from_string(bucket_name, key_name, need_store_string, metadata=None):
-    bucket = get_or_create_bucket(bucket_name, location=Location.DEFAULT)
+def store_data_from_string(bucket_name, key_name, need_store_string, metadata=None, policy='public-read'):
+    bucket = get_or_create_bucket(bucket_name, policy)
     key = bucket.new_key(key_name)
-    key.set_contents_from_string(need_store_string)
+    key.set_contents_from_string(need_store_string, policy=policy)
     if metadata:
         key.metadata.update(metadata)
     return key
 
 def modify_metadata(bucket_name, key_name, metadata):
-    bucket = get_or_create_bucket(bucket_name, location=Location.DEFAULT)
+    bucket = get_or_create_bucket(bucket_name)
     key = bucket.lookup(key_name)
     if key:
         key.copy(bucket.name, key.name, metadata, preserve_acl=True)
     return key
 
 def enable_logging(bucket_name, log_bucket_name, log_prefix=None):
-    bucket = get_or_create_bucket(bucket_name, location=Location.DEFAULT)
+    bucket = get_or_create_bucket(bucket_name)
     log_bucket = s3.lookup(log_bucket_name)
     log_bucket.set_as_logging_target()
     bucket.enable_logging(log_bucket, target_prefix=log_prefix)
 
 def disable_logging(bucket_name):
-    bucket = get_or_create_bucket(bucket_name, location=Location.DEFAULT)
+    bucket = get_or_create_bucket(bucket_name)
     bucket.disable_logging()
 
 def bucket_du(bucket_name):
-    bucket = get_or_create_bucket(bucket_name, location=Location.DEFAULT)
+    bucket = get_or_create_bucket(bucket_name)
     total_bytes = 0
     if bucket:
         for key in bucket:
             total_bytes += key.size
     return total_bytes
 
+def get_expire_data_url(bucket_name, key_name, expires_seconds):#该URL地址有过期时间
+    bucket = get_or_create_bucket(bucket_name)
+    key = bucket.lookup(key_name)
+    if not key:
+        return None
+    return key.generate_url(expires_seconds)
+
+def get_data_url(bucket_name, key_name):
+    domain = 's3.amazonaws.com'
+    url = 'http://%s.s3.amazonaws.com/%s' % (bucket_name, key_name)
+    return url
+
+def set_bucket_acl(bucket_name, policy):
+    '''
+    POLICY: 'private', 'public-read','public-read-write', 
+    'authenticated-read', 'bucket-owner-read', 'bucket-owner-full-control'
+    '''
+    bucket = get_or_create_bucket(bucket_name, policy)
+    bucket.set_acl(policy)
+
+def get_bucket_acl(bucket_name):
+    bucket = get_or_create_bucket(bucket_name)
+    return bucket.get_acl()
+    
