b08c3dc320e97f29446f628be5970a50b89f5910
yongpingxu <jack.wu.xu@gmail.com>
2012-02-21 14:14:59 +0800

    加上注释

--- src/StorageWorkspace/StorageAPI/src/com/scss/db/exception/NoJurisdictionException.java
+++ src/StorageWorkspace/StorageAPI/src/com/scss/db/exception/NoJurisdictionException.java
@@ -1,5 +1,14 @@
 package com.scss.db.exception;
 
+/**
+ * @Title:无权限异常。<br>
+ * @Description: 当用户对所操作资源没有对应的权限时，抛出该异常。<br>
+ * @Deprecated ：
+ * 
+ * @author Jack.wu.xu 2012-2-19
+ * @version 1.0
+ * 
+ */
 public class NoJurisdictionException extends Exception {
 
 }
