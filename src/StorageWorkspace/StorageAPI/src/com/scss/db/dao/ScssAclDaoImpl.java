8f5dea7b07c316e5d27f570ba3906d05933e5861
xuyongping <jack.wu.xu@gmail.com>
2012-02-29 11:00:40 +0800

    增加SameName异常判断

--- src/StorageWorkspace/StorageAPI/src/com/scss/db/dao/ScssAclDaoImpl.java
+++ src/StorageWorkspace/StorageAPI/src/com/scss/db/dao/ScssAclDaoImpl.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.scss.db.connpool.config.IbatisConfig;
 import com.scss.db.exception.SameNameException;
+import com.scss.db.model.ScssAcl;
 import com.scss.db.model.ScssGroup;
 import com.scss.db.model.ScssUser;
 
@@ -21,4 +22,36 @@ public class ScssAclDaoImpl {
 		return instance;
 	}
 
+	public ScssAcl insertAcl(ScssAcl acl) throws SameNameException {
+		try {
+			acl.setId((Long) sqlMap.insert("putAcl", acl));
+		} catch (com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException e) {
+			SameNameException ename = new SameNameException(
+					"Same Acl setting is exist",
+					"Same Acl setting is exist for accessor` "
+							+ acl.getAccessorId() + " on "
+							+ acl.getResourceId());
+			throw ename;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return acl;
+	}
+
+	public ScssAcl getAcl(Long id) {
+		ScssAcl acl = null;
+		try {
+			acl = (ScssAcl) sqlMap.queryForObject("getAcl", id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return acl;
+	}
+
+	public void updateAcl(ScssAcl acl) throws SQLException {
+		sqlMap.update("updateAcl", acl);
+	}
+	public void deleteAcl(ScssAcl acl) throws SQLException {
+		sqlMap.update("deleteAcl", acl);
+	}
 }
